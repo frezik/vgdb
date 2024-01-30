61b04b15a5a84d3e9e71b868f9f617e9bb898485
tmurray <tmurray@wumpus-cave.net>
2024-01-29 18:25:37 -0600

    Scrape wikipedia for NES games

---
+++ scripts/get_nes.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gocolly/colly/v2"
+    "encoding/json"
+)
+
+type Game struct {
+    Name string
+    Publisher string
+    FirstRelease string
+    JPRelease string
+    NARelease string
+    EURelease string
+}
+
+func main() {
+    c := colly.NewCollector(
+        colly.AllowedDomains( "en.wikipedia.org" ),
+    )
+
+    var results []Game
+    c.OnHTML( "#softwarelist tr", func( e *colly.HTMLElement ) {
+        var row_texts []string = e.ChildTexts( "td" )
+        if len( row_texts ) == 0 {
+            return 
+        }
+
+        game := Game{
+            Name: row_texts[0],
+            Publisher: row_texts[1],
+            FirstRelease: row_texts[2],
+            JPRelease: row_texts[3],
+            NARelease: row_texts[4],
+            EURelease: row_texts[5],
+        }
+        results = append( results, game )
+    })
+
+    c.OnScraped( func( r *colly.Response ) {
+        b, err := json.Marshal( results )
+        if err != nil {
+            fmt.Println( "Error encoding to JSON:", err )
+        } else {
+            fmt.Println( string(b) )
+        }
+    })
+
+    c.Visit( "https://en.wikipedia.org/wiki/List_of_Nintendo_Entertainment_System_games" )
+}
