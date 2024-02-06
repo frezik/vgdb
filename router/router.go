23427b57837c899286a9eaf7a06344cdf244f349
tmurray <tmurray@wumpus-cave.net>
2024-02-05 19:15:09 -0600

    Endpoint for listing systems

--- router/router.go
+++ router/router.go
@@ -1,17 +1,28 @@
 package router
 
 import (
+    "encoding/json"
     "net/http"
 
     "github.com/go-chi/chi/v5"
     "github.com/go-chi/chi/v5/middleware"
 )
 
+const data_dir = "data"
+var data_files = map[string]string{
+    "nes": "nes.json",
+    "snes": "snes.json",
+    "sega_genesis": "sega_genesis.json",
+    "sega_master": "sega_master.json",
+}
+
+
 func Routes() *chi.Mux {
     r := chi.NewRouter()
     r.Use( middleware.Logger )
 
     r.Get( "/", HeartBeat )
+    r.Get( "/systems", ListSystems )
 
     return r
 }
@@ -21,4 +32,27 @@ func HeartBeat(
     r *http.Request,
 ) {
     w.Write([]byte("OK"))
+}
+
+func ListSystems(
+    w http.ResponseWriter,
+    r *http.Request,
+) {
+    systems := make( []string, len( data_files ) )
+
+    i := 0
+    for k := range data_files {
+        systems[i] = k
+        i++
+    }
+
+    output := map[string][]string {
+        "systems": systems,
+    }
+
+    err := json.NewEncoder( w ).Encode( output )
+    if err != nil {
+        http.Error( w, "Internal error", http.StatusInternalServerError )
+        return
+    }
 }
--- routes_test.go
+++ routes_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "net/http"
     "net/http/httptest"
     "testing"
@@ -22,5 +23,27 @@ func TestHealthEndpoint( t *testing.T ) {
     if status := rr.Code; status != http.StatusOK {
         t.Errorf( "Handler returned wrong status code. Expected: %d, got: %d",
             http.StatusOK, status )
+    }
+}
+
+func TestSystemsEndpoint( t *testing.T ) {
+    req, err := http.NewRequest( "GET", "/systems", nil )
+
+    if err != nil {
+        t.Errorf( "Error creating new request: %v", err )
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc( router.ListSystems )
+    handler.ServeHTTP( rr, req )
+
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf( "Handler returned wrong status code. Expected: %d, got: %d",
+            http.StatusOK, status )
+    }
+
+    systems := make( map[string][]string )
+    if err := json.NewDecoder( rr.Body ).Decode( &systems ); err != nil {
+        t.Errorf( "Error decoding response body: %v", err )
     }
 }
